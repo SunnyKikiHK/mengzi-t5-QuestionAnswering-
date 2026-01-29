d7c4c66c6489a7b85c53d3e4223b184376d263dd
Sunny <lamyeungkong0108@gmail.com>
2026-01-29 15:55:49 +0800

    Finish until section 'retrieve model'

--- mengzi-t5-base/dataset.py
+++ mengzi-t5-base/dataset.py
@@ -1,9 +1,22 @@
 from torch.utils.data import Dataset 
-from utils import read_json
+from utils import exceed_length_threshold
+
+MAX_DATASET_SIZE = 20000
 
 class MengziT5Dataset(Dataset):
     def __init__(self, data: list):
-        self.data = data 
+        self.data = self.load_data(data) 
+
+    def load_data(self, data: list):
+        filter_num = 0
+        filter_data = []
+        for i, d in enumerate(data):
+            if i >= MAX_DATASET_SIZE or exceed_length_threshold(d):
+                filter_num += 1
+                continue
+            filter_data.append(d)
+        print("Total data filtered away:", filter_num)
+        return filter_data
     
     def __len__(self):
         return len(self.data)
--- mengzi-t5-base/utils.py
+++ mengzi-t5-base/utils.py
@@ -1,6 +1,13 @@
 import json
+import torch
 from tqdm import tqdm
-from transformers import T5Tokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+# Text template 
+PROMPT_Q_TEMPLATE = "问题:" 
+PROMPT_C_TEMPALTE = "上下文:"
+MAX_INPUT_LENGTH = 512
+MAX_TARGET_LENGTH = 128
 
 def read_json(input_file: str) -> list:
     data = []
@@ -16,6 +23,9 @@ def get_data_stats(data: list, tokenizer: T5Tokenizer) -> dict:
         context_lengths.append(len(tokenizer.tokenize(sample["context"])))
         answer_lengths.append(len(tokenizer.tokenize(sample["answer"])))
     return {
+        "question_num": len(question_lengths),
+        "context_num": len(context_lengths),
+        "answer_num": len(answer_lengths),
         "question_mean_length": sum(question_lengths) / len(question_lengths),
         "context_mean_length": sum(context_lengths) / len(context_lengths),
         "answer_mean_length": sum(answer_lengths) / len(answer_lengths),
@@ -23,32 +33,48 @@ def get_data_stats(data: list, tokenizer: T5Tokenizer) -> dict:
         "context_max_length": max(context_lengths),
         "answer_max_length": max(answer_lengths)
         }
+        
 
-def train_collote_fn(batch_samples: list, tokenizer: T5Tokenizer) -> tuple:
-    batch_questions, batch_contexts, batch_answers, batch_ids = [], [], [], []
+def exceed_length_threshold(obj):
+    prompt_input = f"{PROMPT_Q_TEMPLATE}{obj['question']} {PROMPT_C_TEMPALTE}{obj['context']}"
+    if len(prompt_input) > MAX_INPUT_LENGTH or len(obj['answer']) > MAX_TARGET_LENGTH:
+        return True
+    return False 
+
+def collote_fn(batch_samples: list, model: T5ForConditionalGeneration, tokenizer: T5Tokenizer) -> tuple:
+    """
+    Question and context need to be combined as the input of encoder.
+    """
+    batch_inputs , batch_answers, batch_ids = [], [], []
     for sample in batch_samples:
-        batch_questions.append(sample["question"])
-        batch_contexts.append(sample["context"])
+        # input format: "问题:the_question 上下文:the_context"
+        prompt_input = f"{PROMPT_Q_TEMPLATE}{sample['question']} {PROMPT_C_TEMPALTE}{sample['context']}"
+
+        batch_inputs.append(prompt_input)
         batch_answers.append(sample["answer"])
         batch_ids.append(sample["id"])
-    batch_questions = tokenizer(
-        batch_questions,
+    
+    batch_data = tokenizer(
+        batch_inputs,
         padding="max_length",
-        max_length=512,
+        max_length=MAX_INPUT_LENGTH,
         truncation=True,
         return_tensors="pt"
         )
-    batch_contexts = tokenizer(
-        batch_contexts,
-        padding="max_length",
-        max_length=512,
-        truncation=True,
-        return_tensors="pt"
-        )
-    batch_answers = tokenizer(
+    # We don't need to shift right for answers as the T5 from transformers will do that for us.
+    # We don't need the attention mask as the decoder will make it for us.
+    # decoder_input_ids is not necessary as model will also handle that despite it is written here.
+    labels = tokenizer(
         batch_answers,
         padding="max_length",
-        max_length=128,
+        max_length=MAX_TARGET_LENGTH,
         truncation=True,
         return_tensors="pt"
-    )
+    )["input_ids"] # (bsz, seq_len)
+    batch_data['decoder_input_ids'] = model.prepare_decoder_input_ids_from_labels(labels)
+    end_token_index = torch.where(labels == tokenizer.eos_token_id)[1]
+    for i, end_i in enumerate(end_token_index):
+        labels[i][end_i+1:] = -100 #Ignored value for cross entropy
+    batch_data['labels'] = labels
+    return batch_data
+
